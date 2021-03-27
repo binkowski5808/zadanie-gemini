5fd03863ffe5336dc5be7886098ef5b39c2ffb72
Mariusz Bińkowski <binkowski5808@portal.ron.mil.pl>
2021-03-27 09:57:41 +0100

    Simple layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Flex, Text } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import Header from "./ui/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Flex h="100vh" flexDir="column">
+      <Header />
+      <Flex
+        as="main"
+        flex="1 1 100%"
+        justify="center"
+        align="center"
+        flexDir="column"
+      >
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+        <Button>Dodaj do koszyka</Button>
+      </Flex>
+      <Flex as="footer" flex="1 1 auto" justify="center">
+        <Text>Zadanie rekrutacyjne Gemini Design 2021</Text>
+      </Flex>
+    </Flex>
   );
 }
 
---
+++ src/ui/Header.tsx
@@ -0,0 +1,31 @@
+import { Button, IconButton } from "@chakra-ui/button";
+import { useColorMode, useColorModeValue } from "@chakra-ui/color-mode";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Flex, Heading, Link, Spacer } from "@chakra-ui/layout";
+import React from "react";
+import Basket from "../features/basket/Basket";
+
+const Header = () => {
+  const { toggleColorMode } = useColorMode();
+  const colorModeIcon = useColorModeValue(<MoonIcon />, <SunIcon />);
+
+  return (
+    <Flex as="header" flex="1 1 auto" padding={7}>
+      <Heading mr={8}>ZADANIE</Heading>
+      <Flex as="nav" align="center" sx={{ gap: "1rem" }}>
+        <Link>Kategorie</Link>
+        <Link>Coś</Link>
+        <Link>Coś</Link>
+      </Flex>
+      <Spacer />
+      <IconButton
+        aria-label="Zmiana trybu kolorów"
+        icon={colorModeIcon}
+        onClick={toggleColorMode}
+      />
+      <Basket />
+    </Flex>
+  );
+};
+
+export default Header;
